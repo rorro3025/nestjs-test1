bd3a9d53417de73042f90a2c597175af6caa2b25
Rodrigo Hernandez <rorro3025@outlook.com>
2024-03-12 21:35:21 -0600

    cats controller v1 working

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,16 +1,27 @@
-import { Controller, Get, Post} from '@nestjs/common';
+import { Controller, Get, Post, Param, Req, Body } from '@nestjs/common';
+import { Request } from 'express';
 
-const db = ["cat1","cat3"]
+const db = ["cat1", "cat3"]
 
 @Controller('cats')
 export class CatsController {
   @Get()
-    findAll():string[]{
-      return db
+  findAll(@Req() request: Request): string[] {
+    console.log(request.url)
+    return db
   }
 
   @Post()
-    saveOne(){
-      
+  saveOne(@Body() data: { name: string }): string {
+    db.push(data.name)
+    return 'success'
   }
+
+  @Get(":id")
+  getOne(@Param('id') params: string): string {
+    console.log(params)
+    if (db[Number(params)]) return db[Number(params)]
+    return "Not  found"
+  }
+
 }
