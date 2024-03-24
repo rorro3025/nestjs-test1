b3ecf9ac03a363cc0c1b858ead0b1f0189cb1236
Rodrigo Hernandez <rorro3025@outlook.com>
2024-03-24 16:23:40 -0600

    adding correct responses

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,27 +1,53 @@
-import { Controller, Get, Post, Param, Req, Body } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller,Res,HttpStatus, Get, Post, Param, Req, Body, Patch, Delete } from '@nestjs/common';
+import { Request,Response } from 'express';
+import { Cat as CreateCat } from './create-cat.dto';
 
-const db = ["cat1", "cat3"]
+interface Cat {
+  name: string
+  age: number
+}
+let db: Cat[] = [{ name: "cat1", age: 21 }, { name: "cat3", age: 12 }]
 
 @Controller('cats')
 export class CatsController {
   @Get()
-  findAll(@Req() request: Request): string[] {
-    console.log(request.url)
-    return db
+  findAll(@Req() request: Request, @Res() res): Cat[] {
+    return res.status(HttpStatus.ACCEPTED).json(db)
   }
 
   @Post()
-  saveOne(@Body() data: { name: string }): string {
-    db.push(data.name)
-    return 'success'
+  saveOne(@Body() createCat: CreateCat, @Res() res: Response) {
+    db.push(createCat)
+    return res.status(HttpStatus.CREATED)
   }
 
   @Get(":id")
-  getOne(@Param('id') params: string): string {
+  getOne(@Param('id') params: string): Cat | string {
     console.log(params)
     if (db[Number(params)]) return db[Number(params)]
     return "Not  found"
   }
 
+  @Patch(":id")
+  updateOne(@Param("id") params: string, @Body() data: { name: string }) {
+    try {
+      const index = Number(params)
+      if (!db[index]) return "Not found"
+      console.log(db[index]);
+      console.log("data", data)
+      db[index] = { ...db[index], name: data.name }
+      return db[index]
+    } catch (error) {
+      return 'Not valid value'
+    }
+  }
+  @Delete(':id')
+  deleteOne(@Param('id') params: string) {
+    const index = Number(params)
+    if (!db[index]) return 'this element does not exist'
+    delete db[index]
+    const temp = db.filter(i => !!i)
+    db = [...temp]
+    return 'Element deleted'
+  }
 }
